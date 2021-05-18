f1af2fbe4628f19ca1b19c984b9cd6247a976d55
Carina Rodrigues <70609409+rodriguescarinaSI@users.noreply.github.com>
2021-05-18 16:57:32 -0300

    Solved in C
    URI C

---
+++ URI - C/1007 - Diferença.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	
+	int A, B, C, D, diferenca;
+	
+	scanf("%d %d", &A, &B);
+	scanf("%d %d", &C, &D);
+	
+	diferenca = (A * B)-(C * D);
+	
+	printf ("DIFERENCA = %d\n", diferenca);	
+
+ return 0;
+}
