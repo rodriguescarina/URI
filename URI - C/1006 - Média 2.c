4d9661009bb6cd17ec481f8bdf71fbc481f03820
Carina Rodrigues <70609409+rodriguescarinaSI@users.noreply.github.com>
2021-05-18 16:53:17 -0300

    Solved

---
+++ URI - C/1006 - Média 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	
+	double A, B, C, media;
+	
+	scanf ("%lf %lf %lf", &A, &B, &C);
+	
+	media = (((2*A) + (3 *B) + (5 * C)) /10);
+
+    printf ("MEDIA = %.1lf\n", media);	
+
+ return 0;
+}
