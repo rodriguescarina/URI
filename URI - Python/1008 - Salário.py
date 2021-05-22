f49a7ac8127dcb28b6c4dba951fdb8e36be1b703
Carina Rodrigues <70609409+rodriguescarinaSI@users.noreply.github.com>
2021-05-21 21:28:39 -0300

    ...

--- URI - Python/1008 - Salário.py
+++
@@ -1,8 +0,0 @@
-f = int(input())
-horast = int(input())
-valorh = float(input())
-
-salary = (valorh * horast)
-
-print("NUMBER =", f)
-print("SALARY = U$ %0.2f" %salary)
--- URI - Python/1009 - Salário com Bônus.py
+++
@@ -1,7 +0,0 @@
-nome = input()
-salario = float(input())
-vendas = float(input())
-
-total = salario + vendas *0.15
-
-print("TOTAL = R$ %0.2f" %total)
